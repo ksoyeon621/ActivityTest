3889b39456d460a8d965bf71a75042191e955841
ksoyeon621 <ksoyeon621@naver.com>
2017-04-13 20:15:28 +0900

    #3 Button back

--- app/src/main/java/com/example/activitytest/MainActivity.java
+++ app/src/main/java/com/example/activitytest/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but_main = (Button)findViewById(R.id.but_main);
+        but_main.setOnClickListener(butMainHandler);
     }
+
+    //버튼을 눌렀을 때 실행
+    View.OnClickListener butMainHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button but_second=(Button)findViewById(R.id.but_second);
+        but_second.setOnClickListener(butBackHandler);
     }
+
+    View.OnClickListener butBackHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
 }
